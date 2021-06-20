4e420cc4dda1e5fcd778852280c2f15fbd21873a
powerGames1 <86189835+powerGames1@users.noreply.github.com>
2021-06-20 13:34:07 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const Discord = require('discord.js'),
+    client = new Discord.Client({
+        fetchAllMembers: true,
+        partials: ['MESSAGE', 'REACTION']
+    }),
+    config = require('./config.json'),
+    fs = require('fs'),
+    
+    cooldown = new Set()
+ 
+client.login(config.token)
+client.commands = new Discord.Collection()
+client.db = require('./db.json')
+ 
+fs.readdir('./commands', (err, files) => {
+    if (err) throw err
+    files.forEach(file => {
+        if (!file.endsWith('.js')) return
+        const command = require(`./commands/${file}`)
+        client.commands.set(command.name, command)
+    })
+})
+
+client.on('message', message => {
+    if (message.type !== 'DEFAULT' || message.author.bot) return
+ 
+    const args = message.content.trim().split(/ +/g)
+    const commandName = args.shift().toLowerCase()
+    if (!commandName.startsWith(config.prefix)) return
+    const command = client.commands.get(commandName.slice(config.prefix.length))
+    if (!command) return
+    command.run(message, args, client)
+})
+ 
+client.on('guildMemberAdd', member => {
+    member.guild.channels.cache.get(config.greeting.channel).send(`${member}`, new Discord.MessageEmbed()
+        .setDescription(`${member} a rejoint le serveur. Nous sommes désormais ${member.guild.memberCount} ! 🎉`)
+        .setColor('#00ff00'))
+    member.roles.add(config.greeting.role)
+})
+ 
+client.on('guildMemberRemove', member => {
+    member.guild.channels.cache.get(config.greeting.channel).send(new Discord.MessageEmbed()
+        .setDescription(`${member.user.tag} a quitté le serveur... 😢`)
+        .setColor('#ff0000'))
+})
+
+client.on('ready', () => {
+  const statuses = [
+      () => `e*help | e*invite`,
+      () => `${client.guilds.cache.size} serveurs ${client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)} Utilisateur `
+  ]
+  let i = 0
+  setInterval(() => {
+      client.user.setActivity(statuses[i](), {type: 'PLAYING'})
+      i = ++i % statuses.length
+  }, 1e4)
+})
